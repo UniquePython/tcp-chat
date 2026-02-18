00068177e0f756e1b5c3f2842d5903c6ac146c59
Ananyo Bhattacharya <ananyobhattacharya10@gmail.com>
2026-02-18 11:30:33 +0530

    Add binding

--- src/server.c
+++ src/server.c
@@ -14,17 +14,30 @@ void handle_error(const char *err_msg)
 }
 
 
-int main(void)
+int main(int argc, char **argv)
 {
+	if (argc < 2)
+	{
+		printf("Usage: %s <port>\n", argv[0]);
+		exit(EXIT_FAILURE);
+	}
 	int server_sockfd = socket(AF_INET, SOCK_STREAM, 0);
-	if (server_sockfd == -1) handle_error("server sockfd");
+	if (server_sockfd < 1) handle_error("server sockfd");
 	
 	int enable = 1;
 	if (setsockopt(server_sockfd, SOL_SOCKET, SO_REUSEADDR, &enable, sizeof(int)) < 0) {
 		printf("WARNING: setsockopt(SO_REUSEADDR) failed\n");
 	}
 
+	struct sockaddr_in address;
+	memset(&address, 0, sizeof(address));
+	address.sin_family = AF_INET;
+	address.sin_addr.s_addr = INADDR_ANY;
+	address.sin_port = htons(atoi(argv[1]));
+
+	if (bind(server_sockfd, (struct sockaddr *)&address, sizeof(address)) < 0) handle_error("bind");
+
 	printf("SUCCESS!\n");
 
-	return 0;
+	return EXIT_SUCCESS;
 }
