13d20d846d1a943019cb22314a157bd4e613b2cf
Ananyo Bhattacharya <ananyobhattacharya10@gmail.com>
2026-02-18 11:50:24 +0530

    Accept client connection

--- src/server.c
+++ src/server.c
@@ -29,15 +29,22 @@ int main(int argc, char **argv)
 		printf("WARNING: setsockopt(SO_REUSEADDR) failed\n");
 	}
 
-	struct sockaddr_in address;
-	memset(&address, 0, sizeof(address));
-	address.sin_family = AF_INET;
-	address.sin_addr.s_addr = INADDR_ANY;
-	address.sin_port = htons(atoi(argv[1]));
+	struct sockaddr_in server_address;
+	memset(&server_address, 0, sizeof(server_address));
+	server_address.sin_family = AF_INET;
+	server_address.sin_addr.s_addr = INADDR_ANY;
+	server_address.sin_port = htons(atoi(argv[1]));
 
-	if (bind(server_sockfd, (struct sockaddr *)&address, sizeof(address)) < 0) handle_error("bind");
+	if (bind(server_sockfd, (struct sockaddr *)&server_address, sizeof(server_address)) < 0) handle_error("bind");
 
 	if (listen(server_sockfd, atoi(argv[2])) < 0) handle_error("listen");
+
+	struct sockaddr_in client_address;
+	int client_addrlen = sizeof(client_address);
+	int client_sockfd = accept(server_sockfd, (struct sockaddr *)&client_address, (socklen_t*)&client_addrlen);
+	if (client_sockfd < 0) handle_error("accept");
+	printf("Client connected!\n");
+	close(client_sockfd);
 
 	printf("SUCCESS!\n");
 
