85b8dfccd8f1c59af94a480608e7068b5979c59f
Ananyo Bhattacharya <ananyobhattacharya10@gmail.com>
2026-02-18 11:19:43 +0530

    Make socket reusable

--- src/server.c
+++ src/server.c
@@ -18,6 +18,13 @@ int main(void)
 {
 	int server_sockfd = socket(AF_INET, SOCK_STREAM, 0);
 	if (server_sockfd == -1) handle_error("server sockfd");
+	
+	int enable = 1;
+	if (setsockopt(server_sockfd, SOL_SOCKET, SO_REUSEADDR, &enable, sizeof(int)) < 0) {
+		printf("WARNING: setsockopt(SO_REUSEADDR) failed\n");
+	}
+
 	printf("SUCCESS!\n");
+
 	return 0;
 }
